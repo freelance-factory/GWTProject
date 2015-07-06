dcdfadc4ae915144737ce0ec2fecc5b8f89cbc43
Leonardo Riviere <leonardoriviere@gmail.com>
2015-07-06 16:17:43 -0300

    Adding rows to flextable and cleaning

--- src/com/mySampleApplication/client/MySampleApplication.java
+++ src/com/mySampleApplication/client/MySampleApplication.java
@@ -17,6 +17,7 @@ import com.google.gwt.user.client.ui.RootPanel;
  * Entry point classes define <code>onModuleLoad()</code>
  */
 public class MySampleApplication implements EntryPoint {
+    FlexTable a = new FlexTable();
 
     /**
      * This is the entry point method.
@@ -26,7 +27,7 @@ public class MySampleApplication implements EntryPoint {
         final Button button1 = new Button("Click me if you want to pick the car of your dreams.");
         final Label label = new Label();
         final Label label1 = new Label();
-        final FlexTable flexTable = new FlexTable();
+
 //        final CellList cellList = new CellList<>;
 //        final VerticalPanel panel = new VerticalPanel();
 
@@ -50,20 +51,25 @@ public class MySampleApplication implements EntryPoint {
 //                } else {
 //                    label.setText("");
 //                }
-                MySampleApplicationService2.App.getInstance().getList(new AsyncCallback<ArrayList>() {
-                    @Override
-                    public void onSuccess(ArrayList result) {
-                        initTable(result);
-                    }
-                    @Override
-                    public void onFailure(Throwable caught) {
+                if (a.getRowCount() > 0) {
+                    a.clear(true);
+                } else {
+                    MySampleApplicationService2.App.getInstance().getList(new AsyncCallback<ArrayList>() {
+                        @Override
+                        public void onSuccess(ArrayList result) {
+                            initTable(result);
+                        }
 
-                    }
-                });
+                        @Override
+                        public void onFailure(Throwable caught) {
+
+                        }
+                    });
+                }
             }
         });
 
-                // Assume that the host HTML has elements defined whose
+        // Assume that the host HTML has elements defined whose
         // IDs are "slot1", "slot2".  In a real app, you probably would not want
         // to hard-code IDs.  Instead, you could, for example, search for all
         // elements with a particular CSS class and replace them with widgets.
@@ -76,17 +82,16 @@ public class MySampleApplication implements EntryPoint {
     }
 
     private void initTable(ArrayList result) {
-        FlexTable a = new FlexTable();
         FlexTable.FlexCellFormatter cellFormatter = a.getFlexCellFormatter();
         a.setWidth("32em");
         a.setCellPadding(3);
         a.setCellSpacing(5);
-        cellFormatter.setHorizontalAlignment(0,0, HasHorizontalAlignment.ALIGN_CENTER);
-        a.setHTML(0,0, String.valueOf(result.get(0)));
-        cellFormatter.setHorizontalAlignment(1,0, HasHorizontalAlignment.ALIGN_CENTER);
-        a.setHTML(1,0, String.valueOf(result.get(1)));
-        cellFormatter.setHorizontalAlignment(2,0, HasHorizontalAlignment.ALIGN_CENTER);
-        a.setHTML(2,0, String.valueOf(result.get(2)));
+        cellFormatter.setHorizontalAlignment(0, 0, HasHorizontalAlignment.ALIGN_CENTER);
+        a.setHTML(0, 0, String.valueOf(result.get(0)));
+        cellFormatter.setHorizontalAlignment(1, 0, HasHorizontalAlignment.ALIGN_CENTER);
+        a.setHTML(1, 0, String.valueOf(result.get(1)));
+        cellFormatter.setHorizontalAlignment(2, 0, HasHorizontalAlignment.ALIGN_CENTER);
+        a.setHTML(2, 0, String.valueOf(result.get(2)));
         RootPanel.get("slot4").add(a);
     }
 
